d5d729e68507aebcfd921f4518987949947cd0e4
emig23 <emilianogarza002@gmail.com>
2026-09-02 14:17:26 -0500

    feat: added global helper variables to config.py

--- live_webcam/config.py
+++ live_webcam/config.py
@@ -1,3 +1,14 @@
 IMAGENET_MEAN = [0.485, 0.456, 0.406]
 IMAGENET_STD  = [0.229, 0.224, 0.225]
 
+GESTURE_ACTIONS = {
+    "like":    ("Like (Volume Up)",      "volume_up"),
+    "dislike": ("Dislike (Volume Down)", "volume_down"),
+}
+
+HOLD_SECONDS    = 0.5
+COOLDOWN        = 1.0
+VOLUME_STEP     = 0.05
+BRIGHTNESS_STEP = 10
+
+DEFAULT_MODEL_PATH = "../training/content/gesture_model_mobilenet_v2.pth"
